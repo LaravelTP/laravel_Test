0a4430ec7feac8160b09cce5bc91fe78db158b16
wesley-kami <okwuwesley@gmail.com>
2025-07-22 23:14:43 +0100

    création du projet de base

---
+++ database/seeders/StandsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StandsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
